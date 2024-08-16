009a4eee124017cdbb9c18ad437dfba55fe6993b
Jasmine Callon <jascallon99@gmail.com>
2024-08-15 18:29:17 -0700

    print Hello World on I2C LCD screen

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,8 @@
 #include "esp_log.h"
 // TODO: comment
 #include "driver/gpio.h"
+// TODO: comment
+#include <LiquidCrystal_I2C.h>
 
 // Create macro for converting milliseconds to clock ticks
 #define MS_TO_CLOCK_TICK(ms) ms / portTICK_PERIOD_MS
@@ -28,7 +30,7 @@
 // Define what pins are mapped to what peripherals
 //#define PIN_I2C_DISPLAY_GND GND
 //#define PIN_I2C_DISPLAY_VCC VIN
-#define PIN_I2C_DISPLAY_SDA ((gpio_num_t) GPIO_NUM_26)
+#define PIN_I2C_DISPLAY_SDA ((gpio_num_t) GPIO_NUM_25)
 #define PIN_I2C_DISPLAY_SCL ((gpio_num_t) GPIO_NUM_33)
 // Each button is connected to GND, in a pull-up resistor fashion (LOW = 0 = pressed, HIGH = 1 = not pressed)
 // These defines aren't the state of a button, simply:
@@ -42,15 +44,43 @@
 //#define PIN_BUTTON_CONFIRM_OUT GND
 #define PIN_BUTTON_DOWN_IN ((gpio_num_t) GPIO_NUM_32)
 //#define PIN_BUTTON_DOWN_OUT GND
-const gpio_num_t button_in_pins[] = {PIN_BUTTON_UP_IN, PIN_BUTTON_CONFIRM_IN, PIN_BUTTON_DOWN_IN};
+
+// Initialize peripherals
+const gpio_num_t button_in_pins[] = {
+    PIN_BUTTON_UP_IN,
+    PIN_BUTTON_CONFIRM_IN,
+    PIN_BUTTON_DOWN_IN
+};
+LiquidCrystal_I2C display(
+    /* uint8_t lcd_Addr = */ 0x27, // << This depends on your display, see your manufaturer notes!
+    /* uint8_t lcd_cols = */ 20,
+    /* uint8_t lcd_rows = */ 4
+);
 
 // Create link so C can find app_main function from C++ file
 // https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-guides/cplusplus.html
-//extern "C" void app_main()
 void setup()
 {
     // Initialize Arduino serial console
-    Serial.begin(115200);
+    Serial.begin(/* unsigned long baud = */ 115200);
+    // Wait for serial port to connect
+    while(!Serial);
+
+    // Initialize LCD display, clear anything on it, turn on the backlight, and print "Hello world!"
+    // https://lastminuteengineers.com/esp32-i2c-lcd-tutorial/
+    // https://forum.arduino.cc/t/liquidcrystal_i2c-how-to-change-pins/572686/7
+    Wire.begin(
+        /* int sda = */ PIN_I2C_DISPLAY_SDA,
+        /* int sdl = */ PIN_I2C_DISPLAY_SCL
+    );
+    display.init();
+    display.clear();
+    display.backlight();
+    display.setCursor(
+        /* uint8_t col = */ 0,
+        /* uint8_t row = */ 0
+    );
+    display.print(/* const char *c = */"Hello world!");
 
     // Set up 3 GPIO buttons
     // While these buttons are pressed, they will be LOW(0), otherwise they stay HIGH(1)
@@ -71,6 +101,7 @@ void setup()
             /* gpio_num_t gpio_num = */ button_in_pins[i],
             /* gpio_pull_mode_t pull = */ GPIO_PULLUP_ONLY));
 #if 0
+        // Arduino framework no likey
         ESP_ERROR_CHECK_WITHOUT_ABORT(gpio_dump_io_configuration(
             /* FILE *out_stream = */ stdout,
             /* uint64_t io_bit_mask = */ (1UL << button_in_pins[i])));
@@ -95,6 +126,7 @@ void loop()
     Serial.println("Hello world!");
 
     // Show button push status
+    // NOTE: they may not be accurate until a few seconds in and they are pushed at least once
     for(size_t i = 0; i < (sizeof(button_in_pins) / sizeof(*button_in_pins)); ++i)
     {
         Serial.print("GPIO pin ");
